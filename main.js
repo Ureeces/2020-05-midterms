25d56b701b7135ac24853a6c2c1b026259562cb4
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-06 10:50:46 -0400

    Finished capitlizedSentences & isValidPassword

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+/*********************************
+My Code Here
+ ********************************/
+const capitalizeSentences = function(paragraph) {
+  let capitalized = paragraph[0].toUpperCase() + paragraph[1].toLowerCase();
+  
+  for(let i = 2; i < paragraph.length; i++) {
+    let prev = paragraph[i - 2];
+    prev += paragraph[i - 1];
+    
+    let curr = paragraph[i];
+
+    if(prev === ". ") {
+      capitalized += curr.toUpperCase();
+    }
+
+    else {
+      capitalized += curr.toLowerCase();
+    }
+  }
+
+  return capitalized;
+}
+
+const isValidPassword = function(password) {
+  if(password.length < 12) {
+    return false;
+  }
+
+  for(let i = 0; i < password.length; i++) {
+    if(password[i] === " ") {
+      return false;
+    }
+
+  }
+  return true;
+}
 
 
 if (typeof capitalizeSentences === 'undefined') {
