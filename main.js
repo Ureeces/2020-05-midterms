b0d9229c25ae4e9e17275acb7ea4b9c3c95a4d3b
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-06 10:59:25 -0400

    Added new format that includes comment descriptions of each function. Finished halfSquares & countAs

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /*********************************
 My Code Here
  ********************************/
+
+ // capitalizeSentences - capitalizes every sentence after the first in the given paragraph
 const capitalizeSentences = function(paragraph) {
   let capitalized = paragraph[0].toUpperCase() + paragraph[1].toLowerCase();
   
@@ -22,6 +24,9 @@ const capitalizeSentences = function(paragraph) {
   return capitalized;
 }
 
+// isValidPassword - returns false if there is a space in the given string or its length is less than 12 characters,
+// and true otherwise--in other words, a valid password has no spaces and is at least 12 characters, 
+// and this function tells you if the string passed in follows those rules
 const isValidPassword = function(password) {
   if(password.length < 12) {
     return false;
@@ -34,6 +39,35 @@ const isValidPassword = function(password) {
 
   }
   return true;
+}
+
+// makeHalfSquares - returns a new array where every number in the given array has been squared and then halved (divided by two)
+const makeHalfSquares = function(numbers) {
+  const halfSquares = [];
+  
+  for(let i = 0; i < numbers.length; i++) {
+    let currNum = numbers[i];
+    let square = currNum ** 2;
+    let halfSquare = square / 2;
+
+    halfSquares.push(halfSquare);
+  }
+
+  return halfSquares;
+} 
+
+// countAs - returns the number of grades that are at least 90 in the given array
+const countAs = function(grades) {
+  let aCount = 0;
+  
+  for(let i = 0; i < grades.length; i++) {
+    let currGrade = grades[i];
+    if(currGrade >= 90) {
+      aCount++;
+    }
+  }
+
+  return aCount;
 }
 
 
